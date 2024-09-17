74c79e1eb12f20d0476f8ee38b06a6ab0a699a77
Marwin Kirchhofs <m.kirchhofs@gmx.de>
2024-09-17 11:25:40 +0200

    reg file pkg/header update: localparam instead of const variable because of compile order, necessary typedefs, function get_reg_item_from_addr migrated to module, new function reg_file_addr2id

--- reg_file_pkg.sv
+++ reg_file_pkg.sv
@@ -6,7 +6,7 @@
 * 1. the user has to specify axi_reg_file_param.svh, which needs to define 
 * `AXI_LITE_REG_FILE_NUM_REGISTERS and `AXI_LITE_REG_FILE_AXI_REG_FILE_AXI_ADDR_WIDTH
 * 2. the user has to specify axi_reg_file_table.svh, which needs to specify 
-* axi_lite_reg_map_table as `const reg_entry_t [REG_FILE_NUM_REGISTERS]`
+* AXI_LITE_REG_MAP_TABLE as `localparam reg_map_t [REG_FILE_NUM_REGISTERS]`
 * 3. the user is HIGHLY ENCOURAGED to not include axi_reg_file_table.svh 
 * anywhere, and to not include axi_reg_file_param.svh without include guards. If 
 * you need the register file table in the rtl code, import it from reg_file_pkg, 
@@ -58,7 +58,7 @@ package reg_file_pkg;
     localparam          NO_CLEAR_ON_READ    = 1'b0;
 
     localparam          REG_FILE_NUM_REGISTERS  = `AXI_LITE_REG_FILE_NUM_REGISTERS;
-    localparam          REG_FILE_AXI_ADDR_WIDTH          = `AXI_LITE_REG_FILE_AXI_ADDR_WIDTH;
+    localparam          REG_FILE_AXI_ADDR_WIDTH = `AXI_LITE_REG_FILE_AXI_ADDR_WIDTH;
 
     typedef struct packed {
         logic   [`AXI_LITE_REG_FILE_AXI_ADDR_WIDTH-1:0] addr;
@@ -67,38 +67,43 @@ package reg_file_pkg;
         logic                                           clear_on_read;
     } reg_entry_t;
 
-    typedef logic [$clog2(REG_FILE_NUM_REGISTERS)-1:0]  reg_id_t;
+    typedef logic [$clog2(REG_FILE_NUM_REGISTERS)-1:0]  reg_file_id_t;
 
     typedef struct packed {
         logic                                           entry_found;
-        reg_id_t                                        id;
+        reg_file_id_t                                   id;
         reg_entry_t                                     entry;
     } reg_file_item_t;
 
-    `include "axi_reg_file_table.svh"
+    // array type to make defining the register map more straightforward, and 
+    // especially not require that to be an array parameter itself.
+    typedef reg_entry_t reg_map_t [REG_FILE_NUM_REGISTERS];
 
+    `include "axi_reg_file_table.svh"
+    
     /*
-    * be aware: the function does synthesize, but as expected, it can introduce 
-    * a considerable timing problem. You are comparing a 32-bit register against 
-    * a good number of values using LUTs. Already for only 3 integers in the map 
-    * table, that gives 3 hierarchy levels of LUTs for the CE port. Might get 
-* worse with a larger number of choices. Good thing is there is no fanout...
+    * convert an address to the corresponding register file array id.
+    * !!!!!! NO ADDRESS VALIDITY CHECKING !!!!!!!!
+    * IF THE SPECIFIED ADDRESS IS NOT IN THE REGISTER FILE, THE OUTPUT WILL BE 
+    * -1 (WHICH COULD POINT TO A REGISTER).
+    * The reason is to make the return value as simple as possible, assuming 
+    * that the user does have control over their register file axi addresses.  
+        * The function is added to be used in constant statements, so most 
+        * likely one would use parameters for the addr input.
     */
-    function automatic reg_file_item_t get_reg_item_from_addr(
-        logic [REG_FILE_AXI_ADDR_WIDTH-1:0] addr
+    function automatic reg_file_id_t reg_file_addr2id(
+        logic [REG_FILE_AXI_ADDR_WIDTH-1:0] addr,
+        reg_map_t reg_map_table
     );
-        reg_file_item_t          hit;
-        hit.entry_found = 1'b0;
+        reg_file_id_t          id = -1;
 
         for (int i=0; i<REG_FILE_NUM_REGISTERS; i++) begin
-            if (axi_lite_reg_map_table[i].addr == addr) begin
-                hit.entry_found = 1'b1;
-                hit.id = reg_id_t'(i);
-                hit.entry = axi_lite_reg_map_table[i];
+            if (reg_map_table[i].addr == addr) begin
+                id = reg_file_id_t'(i);
             end
         end
 
-        return hit;
+        return id;
     endfunction
 
 endpackage
