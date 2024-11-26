aecba5c34fa1e7c6b49c5e40107ea01a2386b192
Marwin Kirchhofs <m.c.kirchhofs@student.tudelft.nl>
2024-11-26 11:43:13 +0100

    fixes accum_dsp_4_operand - functional in simulation

--- accum_dsp_4_operand.sv
+++ accum_dsp_4_operand.sv
@@ -46,29 +46,29 @@
 * circles. Implications: If set to 1, OUT_REG has no effect (always acts as 
 * OUT_REG=1 because the register is required for accumulation). If 0, 
 * i_accumulate is ignored.
-* :RST_EN: if set to 1, the output reset is available via rst_n (allowing for 
-* resetting an accumulating circuit without deasserting i_accumulate). Has no 
-* effect if not (OUT_REG==1 || ACCUMULATE_EN==1), because in those cases the 
-* register to be reset is deactivated
+* :RST_EN: (NOT IMPLEMENTED) if set to 1, the output reset is available via 
+* rst_n (allowing for resetting an accumulating circuit without deasserting 
+* i_accumulate). Has no effect if not (OUT_REG==1 || ACCUMULATE_EN==1), because 
+* in those cases the register to be reset is deactivated
 * :OPMODE_REG: activates the DSP OPMODEREG, with the effect that i_accumulate 
 * has a 1-cycle latency in taking effect. May help with timing, or with cycle 
 * alignment of certain applications.
 */
 
-import nfe_engine_pkg::*;
+import mcm_math_pkg::*;
 
 module accum_dsp_4_operand #(
-    parameter                                       DATA_WIDTH = DSP_MULT_FULL_WIDTH,
+    parameter                                       DATA_WIDTH = DSP48E2_P_WIDTH,
     parameter                                       RST_EN = 0,
     parameter                                       ACCUMULATE_EN = 1,
     parameter                                       OUT_REG = 1,
     parameter                                       OPMODE_REG = 0
 ) (
-    logic                                           clk,
-    logic                                           rst_n,
-    logic                                           i_accumulate,
-    logic [DATA_WIDTH-1:0]                          i_operands  [4],
-    logic [DATA_WIDTH-1:0]                          o_result
+    input logic                                     clk,
+    input logic                                     rst_n,
+    input logic                                     i_accumulate,
+    input logic [DATA_WIDTH-1:0]                    i_operands  [4],
+    output logic [DATA_WIDTH-1:0]                   o_result
 );
 
     localparam DSP_MASTER_OUT_REG_EN = ((ACCUMULATE_EN == 1) || (OUT_REG == 1)) ? 1 : 0;
@@ -79,12 +79,14 @@ module accum_dsp_4_operand #(
     // INTERNAL SIGNALS
     //----------------------------------------------------------
 
-    logic [DSP_MULT_FULL_WIDTH-1:0]         operands_full_width [4];
+    logic [DSP48E2_P_WIDTH-1:0]             operands_full_width [4];
 
-    logic [DSP_MULT_FULL_WIDTH-1:0]         dsp_cascade;
+    logic [DSP48E2_P_WIDTH-1:0]             dsp_cascade;
     logic                                   dsp_mult_sign;
-    logic [DSP_MULT_IN_WIDTH_WIDE-1:0]      dsp_op_a    [2];
-    logic [DSP_MULT_IN_WIDTH_NARROW-1:0]    dsp_op_b    [2];
+    logic [DSP48E2_P_WIDTH-1:0]             dsp_result;
+    logic [DSP48E2_P_WIDTH-1:0]             dummy_dsp_result;
+    logic [DSP48E2_A_WIDTH-1:0]             dsp_op_a    [2];
+    logic [DSP48E2_B_WIDTH-1:0]             dsp_op_b    [2];
 
     logic                                   dsp_master_accumulate_en;
 
@@ -94,13 +96,15 @@ module accum_dsp_4_operand #(
     // * Y mux: C operand
     // * Z mux: cascade in (master-only)
     // * everything else 0
-    logic [1:0] bits_opmode_w_dsp_master = {1'b0, dsp_master_accumulate_en};
+    logic [1:0] bits_opmode_w_dsp_master;
+    assign bits_opmode_w_dsp_master = {1'b0, dsp_master_accumulate_en};
     const logic [1:0] bits_opmode_w_dsp_slave = 2'b00;
     const logic [1:0] bits_opmode_x_dsp = 2'b11; // necessary for the Y mux
     const logic [1:0] bits_opmode_y_dsp = 2'b11; // select the M (multiplier) register
     const logic [2:0] bits_opmode_z_dsp_master = 3'b001; // select C
     const logic [2:0] bits_opmode_z_dsp_slave = 3'b000; // select C
-    logic [8:0] bits_opmode_dsp_master =
+    logic [8:0] bits_opmode_dsp_master;
+    assign bits_opmode_dsp_master =
         {bits_opmode_w_dsp_master, bits_opmode_z_dsp_master, bits_opmode_y_dsp, bits_opmode_x_dsp};
     const logic [8:0] bits_opmode_dsp_slave =
         {bits_opmode_w_dsp_slave, bits_opmode_z_dsp_slave, bits_opmode_y_dsp, bits_opmode_x_dsp};
@@ -111,8 +115,8 @@ module accum_dsp_4_operand #(
 
 
     // DUMMY DSP CONNECTIONS
-    logic [DSP_MULT_FULL_WIDTH-1:0]         dummy_dsp_slave_result;
-    logic [DSP_MULT_FULL_WIDTH-1:0]         dummy_dsp_cascade;
+    logic [DSP48E2_P_WIDTH-1:0]             dummy_dsp_slave_result;
+    logic [DSP48E2_P_WIDTH-1:0]             dummy_dsp_cascade;
     logic                                   dsp_master_overflow;
     logic                                   dsp_master_underflow;
     logic                                   dsp_master_patterndetect;
@@ -128,14 +132,14 @@ module accum_dsp_4_operand #(
     logic [17:0]                            dummy_dsp_master_bcout;
     logic                                   dummy_dsp_master_carrycascout;
     logic                                   dummy_dsp_master_multsignout;
-    logic [DSP_MULT_FULL_WIDTH-1:0]         dummy_dsp_master_cascadeout;
+    logic [DSP48E2_P_WIDTH-1:0]             dummy_dsp_master_cascadeout;
     logic [3:0]                             dummy_dsp_slave_carryout;
     logic [7:0]                             dummy_dsp_slave_xorout;
     logic [29:0]                            dummy_dsp_slave_acout;
     logic [17:0]                            dummy_dsp_slave_bcout;
     logic                                   dummy_dsp_slave_carrycascout;
     logic                                   dummy_dsp_slave_multsignout;
-    logic [DSP_MULT_FULL_WIDTH-1:0]         dummy_dsp_slave_cascadeout;
+    logic [DSP48E2_P_WIDTH-1:0]             dummy_dsp_slave_cascadeout;
 
 
     //----------------------------------------------------------
@@ -145,21 +149,21 @@ module accum_dsp_4_operand #(
     generate begin: gen_operand_padding
         for (i=0; i<4; i++) begin
             assign operands_full_width[i] =
-                {{(DSP_MULT_FULL_WIDTH-DATA_WIDTH){i_operands[i][DATA_WIDTH-1]}}, i_operands[i]};
+                {{(DSP48E2_P_WIDTH-DATA_WIDTH){i_operands[i][DATA_WIDTH-1]}}, i_operands[i]};
         end
     end endgenerate
 
     assign dsp_master_accumulate_en = (ACCUMULATE_EN == 1) & i_accumulate;
 
     // I know, hard-coded, the signal widths only work with DSP48E2. You have to 
-    // adjust the signal widths should this code ever get in contact with 
-    // a non-Ultrascale DSP.
-    assign dsp_op_a[0] = i_operand[0][DSP_MULT_FULL_WIDTH-1 -: 30];
-    assign dsp_op_b[0] = i_operand[0][17:0];
-    assign dsp_op_a[1] = i_operand[2][DSP_MULT_FULL_WIDTH-1 -: 30];
-    assign dsp_op_b[1] = i_operand[2][17:0];
+        // adjust the signal widths should this code ever get in contact with 
+        // a non-Ultrascale DSP.
+    assign dsp_op_a[0] = operands_full_width[0][DSP48E2_P_WIDTH-1 -: 30];
+    assign dsp_op_b[0] = operands_full_width[0][17:0];
+    assign dsp_op_a[1] = operands_full_width[2][DSP48E2_P_WIDTH-1 -: 30];
+    assign dsp_op_b[1] = operands_full_width[2][17:0];
 
-    // TODO: operand to DSP signal assignment
+    assign o_result = dsp_result[DATA_WIDTH-1:0];
 
     //----------------------------------------------------------
     // SUBMODULES
@@ -204,18 +208,18 @@ module accum_dsp_4_operand #(
         .IS_RSTM_INVERTED(1'b0), // dummy
         .IS_RSTP_INVERTED(1'b1),
         // REGISTER CONTROL ATTRIBUTES
-        .ACASCREG(0), // (dummy)
+        .ACASCREG(1), // (match AREG)
         .ADREG(0), // dummy
         .ALUMODEREG(0), // hard-coded
         .AREG(1), // match slave DSP P REG
-        .BCASCREG(0), // (dummy)
+        .BCASCREG(1), // (match BREG)
         .BREG(1), // match slave DSP P REG
         .CARRYINREG(0), // hard-coded signal
         .CARRYINSELREG(0), // hard-coded signal
         .CREG(1), // match slave DSP P REG
         .DREG(1), // dummy
         .INMODEREG(0), // hard-coded input
-        .MREG(MULT_REG),
+        .MREG(0),
         .OPMODEREG(OPMODE_REG), // hard-coded input
         .PREG(DSP_MASTER_OUT_REG_EN)
     ) inst_dsp48e2_accum_master (
@@ -225,16 +229,16 @@ module accum_dsp_4_operand #(
         .A(dsp_op_a[0]), // operand 0 upper
         .B(dsp_op_b[0]), // operand 0 lower
         // (why 5 leading int bits? 3 from the DSP ALU, 2 from r_in)
-        .C(i_operand[1]),
+        .C(operands_full_width[1]),
         .CARRYIN(1'b0), // necessary because you can't fully disable the ALU carry logic
-        .D({DSP_D_WIDTH{1'b1}}), // dummy
+        .D({DSP48E2_D_WIDTH{1'b1}}), // dummy
         // DATA OUTPUTS
         .CARRYOUT(dummy_dsp_master_carryout),
-        .P(o_result),
+        .P(dsp_result),
         .XOROUT(dummy_dsp_master_xorout),
         // CASCADE INPUTS
-        .ACIN({DSP_A_WIDTH{1'b1}}), // dummy
-        .BCIN({DSP_B_WIDTH{1'b1}}), // dummy
+        .ACIN({DSP48E2_A_WIDTH{1'b1}}), // dummy
+        .BCIN({DSP48E2_B_WIDTH{1'b1}}), // dummy
         .CARRYCASCIN('1), // dummy
         .MULTSIGNIN(dsp_mult_sign), // dummy
         .PCIN(dsp_cascade), // dummy
@@ -250,7 +254,7 @@ module accum_dsp_4_operand #(
         // set up inmode for performing A*B, and routing the input pipeline 
         // according to parameter FULL_INPUT_PIPELINE
         .INMODE(bits_inmode),
-        .OPMODE(bits_opmode_mult),
+        .OPMODE(bits_opmode_dsp_master),
         .ALUMODE(4'b0), // Z+W+X+Y+CIN
         // CONTROL OUTPUTS
         .OVERFLOW(dsp_master_overflow),
@@ -259,12 +263,12 @@ module accum_dsp_4_operand #(
         .UNDERFLOW(dsp_master_underflow),
 
         // RESET/CLOCK ENABLE INPUTS
-        .CEA1(1'b1),
-        .CEA2(1'b0),
+        .CEA1(1'b0),
+        .CEA2(1'b1), // note that A2 is activated if AREG=1
         .CEAD(1'b0),
         .CEC(1'b1),
-        .CEB1(1'b1),
-        .CEB2(1'b0),
+        .CEB1(1'b0),
+        .CEB2(1'b1),
         .CED(1'b0),
         .CEM(1'b0), // (internal multiplier register)
         .CEP(DSP_MASTER_OUT_REG_EN ? 1'b1 : 1'b0),
@@ -338,26 +342,26 @@ module accum_dsp_4_operand #(
         .MREG(0),
         .OPMODEREG(0), // hard-coded input
         .PREG(1) // required for cascade out
-    ) inst_dsp48e2_accum_master (
+    ) inst_dsp48e2_accum_slave (
         .CLK(clk),
 
         // DATA INPUTS
         .A(dsp_op_a[1]), // operand 0 upper
         .B(dsp_op_b[1]), // operand 0 lower
         // (why 5 leading int bits? 3 from the DSP ALU, 2 from r_in)
-        .C(i_operand[3]),
+        .C(operands_full_width[3]),
         .CARRYIN(1'b0), // necessary because you can't fully disable the ALU carry logic
-        .D({DSP_D_WIDTH{1'b1}}), // dummy
+        .D({DSP48E2_D_WIDTH{1'b1}}), // dummy
         // DATA OUTPUTS
         .CARRYOUT(dummy_dsp_slave_carryout),
-        .P(o_result),
+        .P(dummy_dsp_result),
         .XOROUT(dummy_dsp_slave_xorout),
         // CASCADE INPUTS
-        .ACIN({DSP_A_WIDTH{1'b1}}), // dummy
-        .BCIN({DSP_B_WIDTH{1'b1}}), // dummy
+        .ACIN({DSP48E2_A_WIDTH{1'b1}}), // dummy
+        .BCIN({DSP48E2_B_WIDTH{1'b1}}), // dummy
         .CARRYCASCIN('1), // dummy
         .MULTSIGNIN(1'b1), // dummy
-        .PCIN('1), // dummy
+        .PCIN({DSP48E2_P_WIDTH{1'b1}}), // dummy
         // CASCADE OUTPUTS
         .ACOUT(dummy_dsp_slave_acout),
         .BCOUT(dummy_dsp_slave_bcout),
--- mcm_math_pkg.sv
+++ mcm_math_pkg.sv
@@ -1,16 +1,45 @@
 
 package mcm_math_pkg;
 
+    /*
+    * Reverting to stupid binning implementation because questa thinks this is 
+    * not a constant function, due to the use of system function (if I remove 
+    * ceil() and ln(), it stops complaining. From everything I can see, by the 
+    * LRM the function is fine because you could also use ceil() and ln() in 
+    * a constant expression (as we are doing all the time with clog2()). Well, 
+    * there is no way of doing this in a general way without using logarithm.  
+    * Leaving the comment here as a warning. If you figured out why I'm wrong, 
+    * or convinced questa that this is indeed fine, let me know.
+    */
+//     function int accum_tree_get_latency(int num_operands, int accumulate_en, int out_reg);
+//         // TODO: num_levels depend on the implementation type, once there are 
+//         // different implementations
+//         // (basically clog4 - just that doesn't exist)
+//         // TODO: might be unstable, if e.g. ln(4**2) is just not exactly =2*ln(4)
+//         // . If that happens, I'll have to introduce some epsilon, because 
+//         // otherwise it can happen that you actually report a wrong latency, 
+//         // which would be terrible.
+//         automatic int num_levels = ($ceil($ln(num_operands)/$ln(4)));
+//         automatic int combined_out_reg = (accumulate_en || out_reg) ? 1 : 0;
+// 
+//         return num_levels * (1+combined_out_reg);
+//     endfunction
     function int accum_tree_get_latency(int num_operands, int accumulate_en, int out_reg);
-        // TODO: num_levels depend on the implementation type, once there are 
-        // different implementations
-        // (basically clog4 - just that doesn't exist)
-        // TODO: might be unstable, if e.g. ln(4**2) is just not exactly =2*ln(4)
-        // . If that happens, I'll have to introduce some epsilon, because 
-        // otherwise it can happen that you actually report a wrong latency, 
-        // which would be terrible.
-        int num_levels = int'($ceil($ln(num_operands)/$ln(4)));
-        int combined_out_reg = accumulate_en || out_reg ? 1 : 0;
+        automatic int num_levels = 1;
+        automatic int combined_out_reg;
+
+        if (num_operands > 256) begin
+$error("This function does not support more than 256 operands. (Feel free to extend it accordingly)");
+        end
+
+        if (num_operands > 64) begin
+            num_levels = 4;
+        end else if (num_operands > 16) begin
+            num_levels = 3;
+        end else if (num_operands > 4) begin
+            num_levels = 2;
+        end
+        combined_out_reg = (accumulate_en || out_reg) ? 1 : 0;
 
         return num_levels * (1+combined_out_reg);
     endfunction
